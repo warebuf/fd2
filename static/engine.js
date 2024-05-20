9666b535a57e99b0d60608ca8e473b2059b335cc
EC2 Default User <ec2-user@ip-172-31-21-105.us-east-2.compute.internal>
2024-05-20 00:42:44 +0000

    added a html button on canvas

--- static/engine.js
+++ static/engine.js
@@ -3,19 +3,36 @@ console.log("loaded js");
 var c = document.getElementById("myCanvas");
 const ctx = c.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 20, 150, 100);
-ctx.stroke();
+//c.width = window.innerWidth;
+//c.height = window.innerHeight;
+
+c.width = document.body.clientWidth;
+c.height = document.body.clientHeight;
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || function(f){return setTimeout(f, 1000/60)};
+window.cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame || function(requestID){clearTimeout(requestID)};
 
 
 var x = 200;
+var y = 200;
+var dx = 4;
+var dy = 4;
+var radius = 30;
 function anime() {
-	requestAnimationFrame(anime);
-	c.beginPath();
-	c.arc(x,200,30,0,Math.PI*2,false);
-	c.strokeStyle='blue';
-	c.stroke();
-	x += 1;
-	console.log("hi")
+	window.requestAnimationFrame(anime);
+	//ctx.clearRect(0,0,innerWidth,innerHeight);
+	ctx.beginPath();
+	ctx.arc(x,y,radius,0,Math.PI*2,false);
+	ctx.strokeStyle='blue';
+	ctx.stroke();
+	
+	if (x + radius > c.width || x - radius < 0) {
+		dx = -dx;
+	}
+	if (y + radius > c.height || y - radius < 0) {
+		dy = -dy;
+	}
+	x += dx;
+	y += dy;
 }
 anime()

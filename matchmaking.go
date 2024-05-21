c929b0f7c30a2ba5fa4338d981bed3e1ba54c9d0
warebuf <13jeremyjlee@gmail.com>
2024-05-20 23:35:10 -0600

    f

--- matchmaking.go
+++ matchmaking.go
@@ -127,7 +127,8 @@ func (m *match) run() {
 			uid_to_user.mutex.Lock()
 			ws.u.mutex.Lock()
 			if check == false {
-				fmt.Println(ws.u.mid_to_match)
+				fmt.Println("1", ws.u.mid_to_match)
+				fmt.Println("2", ws.u)
 				ws.u.mid_to_match[m.mid] = m
 				ws.u.mid_to_msid_to_match_socket[m.mid] = make(map[uuid.UUID]*match_socket)
 			}

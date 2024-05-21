4bba97d624b8ca13420879f0f7060162b172d77e
warebuf <13jeremyjlee@gmail.com>
2024-05-21 13:15:27 -0600

    asd

--- matchmaking.go
+++ matchmaking.go
@@ -126,7 +126,7 @@ func (m *match) run() {
 			if check == false {
 				fmt.Println("1", ws.u.mid_to_match)
 				fmt.Println("2", ws.u.mid_to_match[m.mid])
-				fmt.Println("3", m)
+				fmt.Printf("3 %+v\n", m)
 
 				ws.u.mid_to_match[m.mid] = m
 				ws.u.mid_to_msid_to_match_socket[m.mid] = make(map[uuid.UUID]*match_socket)

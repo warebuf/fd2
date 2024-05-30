e0e4eb02a602d02ff001a14ba2cfe213d585f238
warebuf <13jeremyjlee@gmail.com>
2024-05-30 17:35:00 -0600

    a

--- static/engine.js
+++ static/engine.js
@@ -23,9 +23,9 @@ function anime() {
     let start = Date.now().toLocaleString('en-CH');
     c.fillStyle = 'black';
     ctx.fillRect((c.width/2)-(ctx.measureText(start).width/2),(c.height/2)-ctx.measureText('M').width,ctx.measureText(start).width,ctx.measureText('M').width);
-    //c.fillStyle = 'white';
-    //c.fillText(start,(c.width/2) - (c.measureText(start).width/2), c.height/2);
-    //ctx.clearRect(0,0,innerWidth,innerHeight);
+    c.fillStyle = 'white';
+    ctx.fillText(start,(c.width/2) - (ctx.measureText(start).width/2), c.height/2);
+    ctx.clearRect(0,0,innerWidth,innerHeight);
 }
 anime()
 

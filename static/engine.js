92b5b852621c1759c985622d7ca138712e24937d
warebuf <13jeremyjlee@gmail.com>
2024-05-30 17:50:21 -0600

    a

--- static/engine.js
+++ static/engine.js
@@ -20,13 +20,13 @@ var dy = 4;
 var radius = 30;
 function anime() {
     window.requestAnimationFrame(anime);
-    //ctx.clearRect(0,0,innerWidth,innerHeight);
+    ctx.clearRect(0,0,innerWidth,innerHeight);
 
     let start = Date.now().toLocaleString('en-CH');
     //c.fillStyle = 'black';
     //ctx.fillRect((c.width/2)-(ctx.measureText(start).width/2),(c.height/2)-ctx.measureText('M').width,ctx.measureText(start).width,ctx.measureText('M').width);
     ctx.fillStyle = 'blue';
-    ctx.font = '10px Arial';
+    ctx.font = '12px Arial';
     ctx.fillText(start,100, 100);
 }
 anime()

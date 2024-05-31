1fe993d3cb477ed9d142fc380bd7e5a8b2615b4f
warebuf <13jeremyjlee@gmail.com>
2024-05-30 18:06:21 -0600

    a

--- static/engine.js
+++ static/engine.js
@@ -20,7 +20,7 @@ var dy = 4;
 var radius = 30;
 function anime() {
     window.requestAnimationFrame(anime);
-    ctx.fillStyle = '#888888';
+    ctx.fillStyle = 'red';
     ctx.clearRect(0,0,innerWidth,innerHeight);
 
     let start = Date.now().toLocaleString('en-CH');

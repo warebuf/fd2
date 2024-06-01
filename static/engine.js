e50cf83aa18e8469d5dbd65f5739ff346b2cf579
warebuf <13jeremyjlee@gmail.com>
2024-05-31 23:01:11 -0600

    a

--- static/engine.js
+++ static/engine.js
@@ -47,8 +47,9 @@ function anime() {
         ctx.font = '12px Arial';
         ctx.fillStyle = 'white';
         ctx.fillText(startCount,(c.width/2) - (ctx.measureText(start).width/2), 20);
-        console.log(1, startCount, start)
-        console.log(2, startCount, start.replace("’",""))
+        console.log(1, start.replace("’",""))
+        console.log(1, start.replace("’","").replace("’",""))
+
 
     }
 

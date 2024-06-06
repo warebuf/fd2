56b4fa437b02f541f73e6a629612c2d2170d19af
warebuf <13jeremyjlee@gmail.com>
2024-06-06 17:34:06 -0600

    a

--- static/engine.js
+++ static/engine.js
@@ -88,7 +88,7 @@ function drawPos() {
                         Math.round(match_data[current_state][i][j][k].Position)) +
                         (match_data[current_state][i][j][k].Direction ? '>' : '<') +
                         road.substring(Math.round(match_data[current_state][i][j][k].Position) + 1);
-                    ctx.fillText(road, 250, ((i+j)*115) + 82 + (k*20));
+                    ctx.fillText(road, 350, ((i+j)*115) + 82 + (k*20));
                 }
             }
 
@@ -105,25 +105,25 @@ function drawState() {
                 } else {
                     ctx.fillStyle = 'red';
                 }
-                ctx.fillRect(225, ((i+j)*115) + 71 + (k*20), 5, 5);
+                ctx.fillRect(325, ((i+j)*115) + 71 + (k*20), 5, 5);
                 if (true) {
                     c.fillStyle = 'green';
                 } else {
                     c.fillStyle = 'red';
                 }
-                ctx.fillRect(220, ((i+j)*115) + 76 + (k*20), 5, 7); // make it 270,5,7 OR 269,6,4
+                ctx.fillRect(320, ((i+j)*115) + 76 + (k*20), 5, 7); // make it 270,5,7 OR 269,6,4
                 if (true) {
                     ctx.fillStyle = 'green';
                 } else {
                     ctx.fillStyle = 'red';
                 }
-                ctx.fillRect(230, ((i+j)*115) + 76 + (k*20), 5, 7);
+                ctx.fillRect(330, ((i+j)*115) + 76 + (k*20), 5, 7);
                 if (true) {
                     ctx.fillStyle = 'green';
                 } else {
                     c.fillStyle = 'red';
                 }
-                ctx.fillRect(225, ((i+j)*115) + 81 + (k*20), 5, 7);
+                ctx.fillRect(325, ((i+j)*115) + 81 + (k*20), 5, 7);
             }
         }
     }

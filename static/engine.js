833f2e2b39da37d15916bf1986a47cb8250fb268
warebuf <13jeremyjlee@gmail.com>
2024-06-13 18:11:35 -0600

    asd

--- static/engine.js
+++ static/engine.js
@@ -37,10 +37,10 @@ function anime() {
     if(startCount != null) {
         remaining_time = startCount - start.replaceAll("’","")
         ctx.fillStyle = 'black';
-        ctx.fillRect((c.width/2)-(ctx.measureText(remaining_time).width/2),20,ctx.measureText(remaining_time).width,ctx.measureText('M').width);
+        ctx.fillRect((c.width/2)-(ctx.measureText(remaining_time).width/2),25,ctx.measureText(remaining_time).width,ctx.measureText('M').width);
         ctx.font = '12px Arial';
         ctx.fillStyle = 'white';
-        ctx.fillText(remaining_time,(c.width/2) - (ctx.measureText(remaining_time).width/2), 30);
+        ctx.fillText(remaining_time,(c.width/2) - (ctx.measureText(remaining_time).width/2), 35);
 
         // need to change it so that it not only sends ur input when time expires, but sends it if you disconnect
         if(remaining_time<=0 && once) {

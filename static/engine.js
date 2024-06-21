14114fb614c47a1c698ef5d67bdeaf656a3d2d9d
warebuf <13jeremyjlee@gmail.com>
2024-06-21 11:07:06 -0600

    start of animation commits

--- static/engine.js
+++ static/engine.js
@@ -133,12 +133,23 @@ function drawPos() {
                         for(let k = 0; k < state[i][j].length; k++) {
 
                             // if we already set a move on this unit, but the user requests a change on it, and the unit is at the starting point
+
                             if(
                                 (state[i][j][k].Position == 0) &&
                                 (state[i][j][k].Direction == 1) &&
                                 (state[i][j][k].Position == match_data[animating_state][i][j][k].Position) 
                             ) {
                                 state[i][j][k].Move = match_data[animating_state][i][j][k].Move
+                            }
+                            else if( // if the unit is waiting for a move, but no new move is in
+                                (state[i][j][k].Position == 0) &&
+                                (state[i][j][k].Direction == 0) &&
+                                (state[i][j][k].Move == -1) &&
+                                (state[i][j][k].Move == match_data[animating_state][i][j][k].Move)
+                            ) {
+                                if(time_queue.length > 0) {
+                                    event_log.push(time_queue.shift())
+                                }
                             }
                             else if( // if the unit is waiting for a move, and a new move comes in
                                 (state[i][j][k].Position == 0) &&

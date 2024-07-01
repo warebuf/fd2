42255d22f87a1283a76c9ce5eba6872a86eb17d9
warebuf <13jeremyjlee@gmail.com>
2024-06-30 21:09:59 -0600

    START OF DRAFT FORMAT

--- main.go
+++ main.go
@@ -814,9 +814,7 @@ func psocketSetupHandler(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("GOT HERE")
 	// when a match socket is created, send them a list of all possible matches
 	for _, i := range plid_to_permission_list.global {
-		x := &pmessage{Event: "newMatch", Message: i.game_mode + strconv.Itoa(int(i.capacity)), When: time.Now(), PLID: i.plid} // send match
-		fmt.Println(x)
-		temp.incoming_message <- x
+		temp.incoming_message <- &pmessage{Event: "newPL", Message: i.game_mode + strconv.Itoa(int(i.capacity)), When: time.Now(), PLID: i.plid} // send match
 		for _, j := range i.gamer_permission_list {
 			temp.incoming_message <- &pmessage{Name: j.email, Message: "participantJoinSuccess", Event: "participantJoinSuccess", When: time.Now(), PLID: i.plid} // send users in match
 		}

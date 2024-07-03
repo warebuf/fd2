d0a8c31001518ba635002982489e5305ecdb88ab
warebuf <13jeremyjlee@gmail.com>
2024-07-02 23:54:20 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -648,13 +648,13 @@ function drawBench() {
             ctx.textAlign = "left";
             ctx.fillStyle = 'white';
             ctx.font = '11px monospace';
-            ctx.fillText("▼",60, 700);
+            ctx.fillText("▼",70, 700);
         }
         if(h_index > 0) {
             ctx.textAlign = "left";
             ctx.fillStyle = 'white';
             ctx.font = '11px monospace';
-            ctx.fillText("▲",60, 500);
+            ctx.fillText("▲",70, 500);
         }
 
     }

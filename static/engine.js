706808c9350d5a94ee8bc2f60cc2d677ad484d02
warebuf <13jeremyjlee@gmail.com>
2024-07-02 23:32:40 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -641,7 +641,7 @@ function drawBench() {
 
             ctx.textAlign = "left";
             ctx.fillStyle = 'green';
-            ctx.fillRect(100+ a,700+b,3,3);
+            ctx.fillRect(50+ a,700+b,3,3);
         }
 
     }

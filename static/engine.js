461a2e4414181f2b91eab24bb30fbe90745ea271
warebuf <13jeremyjlee@gmail.com>
2024-07-02 23:59:26 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -585,7 +585,8 @@ function drawStarters() {
         for(let i = 0; i < state[my_team][my_int].length; i++) {
             ctx.font = '11px monospace';
             ctx.fillStyle = 'white';
-            ctx.fillText(state[my_team][my_int][i],0, 0 + (10*i));
+            ctx.textAlign = "left";
+            ctx.fillText(state[my_team][my_int][i]H.HP,0, 0 + (10*i));
         }
     }
 }

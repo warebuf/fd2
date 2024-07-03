76c01f35c3ea379cc3655d83ff38e0bf08cdce23
warebuf <13jeremyjlee@gmail.com>
2024-07-03 12:23:05 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -625,8 +625,8 @@ function drawStarters() {
             ctx.fillStyle = 'red';
             ctx.textAlign = "left";
             ctx.fillRect(15,90 + (60*i), 1, 55);
-            ctx.fillRect(15,90 + (60*i), 10, 1);
-            ctx.fillRect(15,90 + 55 + (60*i), 10, 1);
+            ctx.fillRect(15,90 + (60*i), 5, 1);
+            ctx.fillRect(15,90 + 55 + (60*i), 5, 1);
             // right bracket
 
             // HEAD STARTERS

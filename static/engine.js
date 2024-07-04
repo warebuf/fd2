5c7e109d161ba5e7962a6166e974e5e8cb1ccbfa
warebuf <13jeremyjlee@gmail.com>
2024-07-03 18:36:35 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -920,7 +920,7 @@ function drawBench() {
 
         let x = 60
         let y1 = 350
-        let x_offset = 75
+        let x_offset = 175
 
         ctx.font = '11px monospace';
         ctx.fillStyle = 'white';

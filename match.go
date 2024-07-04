a6095ebdc826af5ac6fdf543613ee410dbce2108
warebuf <13jeremyjlee@gmail.com>
2024-07-04 00:39:08 -0600

    working on char sel

--- match.go
+++ match.go
@@ -269,13 +269,14 @@ func m_read(m *match_socket) {
 							tp_index := m.m.uuid_to_team_int[m.u.uid].ab
 							unit_index_1, _ := strconv.Atoi(indices[2])
 							temp := *m.m.benchH[tp_index][unit_index_1]
-							fmt.Println(temp)
 							team_index := m.m.uuid_to_team_int[m.u.uid].a
 							player_index := m.m.uuid_to_team_int[m.u.uid].b
 							unit_index_2, _ := strconv.Atoi(indices[5])
 							m.m.benchH[tp_index][unit_index_1] = &m.m.team_client_hero[team_index][player_index][unit_index_2].H
-							fmt.Println(temp)
+							fmt.Println(m.m.benchH[tp_index][unit_index_1])
 							m.m.team_client_hero[team_index][player_index][unit_index_2].H = temp
+							fmt.Println(m.m.benchH[tp_index][unit_index_1])
+
 						} else if indices[0] == "1" && indices[3] == "1" { // both are bench
 							fmt.Println("GOTHERE4")
 							tp_index := m.m.uuid_to_team_int[m.u.uid].ab

20c85f7b9f5c06275675ed1d48ce284d14cfe7c9
warebuf <13jeremyjlee@gmail.com>
2024-07-05 15:11:09 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -1401,7 +1401,7 @@ function drawTable1() {
         ctx.fillRect(x_offset+110,y_offset+13,ctx.measureText(state[my_team][my_int][0].H.NAME).width,ctx.measureText('M').width);
         ctx.fillStyle = 'white';
         ctx.fillText(state[my_team][my_int][0].H.NAME,x_offset+110, y_offset+20 )
-        ctx.fillText("A basic close attack.",x_offset+110, y_offset+30 )
+        ctx.fillText("Basic close attack.",x_offset+110, y_offset+30 )
 
         ser   = state[my_team][my_int][0].L.SERIAL.toString()
         for (let k = ser.length; k < 7; k++) {
@@ -1413,7 +1413,7 @@ function drawTable1() {
         ctx.fillRect(x_offset+110,y_offset+63,ctx.measureText(state[my_team][my_int][0].L.NAME).width,ctx.measureText('M').width);
         ctx.fillStyle = 'white';
         ctx.fillText(state[my_team][my_int][0].L.NAME,x_offset+110, y_offset+70 )
-        ctx.fillText("A basic close attack.",x_offset+110, y_offset+80 )
+        ctx.fillText("Basic close attack.",x_offset+110, y_offset+80 )
 
         ser   = state[my_team][my_int][0].R.SERIAL.toString()
         for (let k = ser.length; k < 7; k++) {
@@ -1425,7 +1425,7 @@ function drawTable1() {
         ctx.fillRect(x_offset+110,y_offset+113,ctx.measureText(state[my_team][my_int][0].R.NAME).width,ctx.measureText('M').width);
         ctx.fillStyle = 'white';
         ctx.fillText(state[my_team][my_int][0].R.NAME,x_offset+110, y_offset+120 )
-        ctx.fillText("A basic close attack.",x_offset+110, y_offset+130 )
+        ctx.fillText("Basic close attack.",x_offset+110, y_offset+130 )
 
         ser   = state[my_team][my_int][0].B.SERIAL.toString()
         for (let k = ser.length; k < 7; k++) {
@@ -1437,7 +1437,7 @@ function drawTable1() {
         ctx.fillRect(x_offset+110,y_offset+163,ctx.measureText(state[my_team][my_int][0].B.NAME).width,ctx.measureText('M').width);
         ctx.fillStyle = 'white';
         ctx.fillText(state[my_team][my_int][0].B.NAME,x_offset+110, y_offset+170 )
-        ctx.fillText("A basic close attack.",x_offset+110, y_offset+180 )
+        ctx.fillText("Basic close attack.",x_offset+110, y_offset+180 )
 
     }
 }

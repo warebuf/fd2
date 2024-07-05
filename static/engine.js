506cadbf8b6f2e1128cc786e9b047ef3f7d17832
warebuf <13jeremyjlee@gmail.com>
2024-07-05 15:31:06 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -1457,19 +1457,19 @@ function drawTable2() {
         ctx.textAlign = "center";
         ctx.fillRect(x_offset-2+0,y_offset - (0 | state[my_team][my_int][0].H.HP/2),4,0 | state[my_team][my_int][0].H.HP/2);
         ctx.fillText("HP",x_offset, y_offset+10 )
-        ctx.fillRect(x_offset-2+20,y_offset - (0 | state[my_team][my_int][0].H.ATK/2),4,0 | state[my_team][my_int][0].H.ATK/2);
-        ctx.fillText("ATK",x_offset+20, y_offset+10 )
-        ctx.fillRect(x_offset-2+40,y_offset - (0 | state[my_team][my_int][0].H.DEF/2),4,0 | state[my_team][my_int][0].H.DEF/2);
-        ctx.fillText("DEF",x_offset+40, y_offset+10 )
-        ctx.fillRect(x_offset-2+60,y_offset - (0 | state[my_team][my_int][0].H.ACC/2),4,0 | state[my_team][my_int][0].H.ACC/2);
-        ctx.fillText("ACC",x_offset+60, y_offset+10 )
-        ctx.fillRect(x_offset-2+80,y_offset - (0 | state[my_team][my_int][0].H.CRT/2),4,0 | state[my_team][my_int][0].H.CRT/2);
-        ctx.fillText("CRT",x_offset+80, y_offset+10 )
-        ctx.fillRect(x_offset-2+100,y_offset - (0 | state[my_team][my_int][0].H.CD/2),4,0 | state[my_team][my_int][0].H.CD/2);
-        ctx.fillText("CD",x_offset+100, y_offset+10 )
-        ctx.fillRect(x_offset-2+120,y_offset - (0 | state[my_team][my_int][0].H.CLU/2),4,0 | state[my_team][my_int][0].H.CLU/2);
-        ctx.fillText("CLU",x_offset+120, y_offset+10 )
-        ctx.fillRect(x_offset-2+140,y_offset - (0 | state[my_team][my_int][0].H.Weight/2),4,0 | state[my_team][my_int][0].H.Weight/2);
-        ctx.fillText("WGT",x_offset+140, y_offset+10 )
+        ctx.fillRect(x_offset-2+30,y_offset - (0 | state[my_team][my_int][0].H.ATK/2),4,0 | state[my_team][my_int][0].H.ATK/2);
+        ctx.fillText("ATK",x_offset+30, y_offset+10 )
+        ctx.fillRect(x_offset-2+60,y_offset - (0 | state[my_team][my_int][0].H.DEF/2),4,0 | state[my_team][my_int][0].H.DEF/2);
+        ctx.fillText("DEF",x_offset+60, y_offset+10 )
+        ctx.fillRect(x_offset-2+90,y_offset - (0 | state[my_team][my_int][0].H.ACC/2),4,0 | state[my_team][my_int][0].H.ACC/2);
+        ctx.fillText("ACC",x_offset+90, y_offset+10 )
+        ctx.fillRect(x_offset-2+120,y_offset - (0 | state[my_team][my_int][0].H.CRT/2),4,0 | state[my_team][my_int][0].H.CRT/2);
+        ctx.fillText("CRT",x_offset+120, y_offset+10 )
+        ctx.fillRect(x_offset-2+150,y_offset - (0 | state[my_team][my_int][0].H.CD/2),4,0 | state[my_team][my_int][0].H.CD/2);
+        ctx.fillText("CD",x_offset+150, y_offset+10 )
+        ctx.fillRect(x_offset-2+180,y_offset - (0 | state[my_team][my_int][0].H.CLU/2),4,0 | state[my_team][my_int][0].H.CLU/2);
+        ctx.fillText("CLU",x_offset+180, y_offset+10 )
+        ctx.fillRect(x_offset-2+210,y_offset - (0 | state[my_team][my_int][0].H.Weight/2),4,0 | state[my_team][my_int][0].H.Weight/2);
+        ctx.fillText("WGT",x_offset+210, y_offset+10 )
     }
 }

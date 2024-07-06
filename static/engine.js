670d1d0118003382c488c3e73c84e4509dc6f9bc
warebuf <13jeremyjlee@gmail.com>
2024-07-06 10:56:11 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -1887,10 +1887,10 @@ function drawStartersTable() {
                     ctx.fillRect(x_offset-2-20+(user_shift*user_count),y_offset - (0 | state[j][k][i].R.HP/2) + (i*unit_shift),4,0 | state[j][k][i].R.HP/2);
                     ctx.fillRect(x_offset-2-10+(user_shift*user_count),y_offset - (0 | state[j][k][i].B.HP/2) + (i*unit_shift),4,0 | state[j][k][i].B.HP/2);
                     ctx.fillText("HP",x_offset-25+(user_shift*user_count), y_offset+15 + (i*unit_shift) )
-                    //ctx.fillText(state[j][k][i].H.HP,x_offset-2-40, y_offset+25 + (i*unit_shift) )
+                    ctx.fillText(state[j][k][i].H.HP,x_offset-40+(user_shift*user_count), y_offset+25 + (i*unit_shift) )
                     ctx.fillText(state[j][k][i].L.HP,x_offset-30+(user_shift*user_count), y_offset+25 + (i*unit_shift) )
-                    //ctx.fillText(state[j][k][i].R.HP,x_offset-2-20, y_offset+25 + (i*unit_shift) )
-                    //ctx.fillText(state[j][k][i].B.HP,x_offset-2-10, y_offset+25 + (i*unit_shift) )
+                    ctx.fillText(state[j][k][i].R.HP,x_offset-20+(user_shift*user_count), y_offset+25 + (i*unit_shift) )
+                    ctx.fillText(state[j][k][i].B.HP,x_offset-10+(user_shift*user_count), y_offset+25 + (i*unit_shift) )
 
                     ctx.fillRect(x_offset-2+10+(user_shift*user_count),y_offset - (0 | state[j][k][i].H.ATK/2) + (i*unit_shift),4,0 | state[j][k][i].H.ATK/2);
                     ctx.fillRect(x_offset-2+20+(user_shift*user_count),y_offset - (0 | state[j][k][i].L.ATK/2) + (i*unit_shift),4,0 | state[j][k][i].L.ATK/2);

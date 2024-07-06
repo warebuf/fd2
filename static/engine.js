e23d709539063eb99529918f213ea23639a38226
warebuf <13jeremyjlee@gmail.com>
2024-07-05 22:29:25 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -1516,10 +1516,10 @@ function drawStartersTable() {
         ctx.fillRect(x_offset-2-10,y_offset - (0 | state[my_team][my_int][0].B.HP/2),4,0 | state[my_team][my_int][0].B.HP/2);
         ctx.fillText("HP",x_offset-25, y_offset+10 )
         ctx.font = '5px monospace';
-        ctx.fillText(state[my_team][my_int][0].H.HP,x_offset-40, y_offset+20 )
-        ctx.fillText(state[my_team][my_int][0].L.HP,x_offset-30, y_offset+20 )
-        ctx.fillText(state[my_team][my_int][0].R.HP,x_offset-20, y_offset+20 )
-        ctx.fillText(state[my_team][my_int][0].B.HP,x_offset-10, y_offset+20 )
+        ctx.fillText(state[my_team][my_int][0].H.HP,x_offset-40, y_offset+20 - (0 | state[my_team][my_int][0].H.HP/2) )
+        ctx.fillText(state[my_team][my_int][0].L.HP,x_offset-30, y_offset+20 - (0 | state[my_team][my_int][0].L.HP/2) )
+        ctx.fillText(state[my_team][my_int][0].R.HP,x_offset-20, y_offset+20 - (0 | state[my_team][my_int][0].R.HP/2) )
+        ctx.fillText(state[my_team][my_int][0].B.HP,x_offset-10, y_offset+20 - (0 | state[my_team][my_int][0].B.HP/2) )
 
         ctx.fillRect(x_offset-2+30,y_offset - (0 | state[my_team][my_int][0].H.ATK/2),4,0 | state[my_team][my_int][0].H.ATK/2);
         ctx.fillText("ATK",x_offset+30, y_offset+10 )

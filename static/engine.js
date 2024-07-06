01114115b25f68122b0beb065556f4428500cc63
warebuf <13jeremyjlee@gmail.com>
2024-07-06 14:05:30 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -263,8 +263,7 @@ function drawPos() {
                             let y1 = ((i+j)*115) + 98 + (k*20)
                             let x2_x1 = (x2 - x1)*perc
                             let y2_y1 = (y2 - y1)*perc
-                            //dashedLine(x1,y1,x1+x2_x1,y1+y2_y1,[5,2])
-                            dashedLine(x1,y1,x2,y2,[5,2])
+                            dashedLine(x1,y1,x1+x2_x1,y1+y2_y1,[5,2])
 
                             if(draw_attacks>=30) {
                                 ctx.fillStyle = 'white'

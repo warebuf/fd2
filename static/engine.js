1329bac71f62b1fe1e9756c876f2f01ab5f72f1c
warebuf <13jeremyjlee@gmail.com>
2024-07-05 21:52:05 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -1486,8 +1486,8 @@ function drawTable2() {
         }
         ser = 'B' + ser
         ctx.textAlign = "left";
-        ctx.fillText(ser,x_offset-10+0, y_offset+30 )
-        ctx.fillText(state[my_team][my_int][0].H.NAME,x_offset-10+0, y_offset+40 )
+        ctx.fillText(ser,x_offset-10+0, y_offset+30-130 )
+        ctx.fillText(state[my_team][my_int][0].H.NAME,x_offset-10+0, y_offset+40-120 )
 
 
     }

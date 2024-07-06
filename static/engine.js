ce86c1a70d1cfadf4b6cbd300a76874448f2d152
warebuf <13jeremyjlee@gmail.com>
2024-07-05 22:35:35 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -1516,9 +1516,17 @@ function drawStartersTable() {
         ctx.fillRect(x_offset-2-10,y_offset - (0 | state[my_team][my_int][0].B.HP/2),4,0 | state[my_team][my_int][0].B.HP/2);
         ctx.fillText("HP",x_offset-25, y_offset+10 )
         ctx.font = '8px monospace';
+        if(state[my_team][my_int][0].H.HP==100){ctx.font = '5px monospace';}
+        else{ctx.font = '8px monospace';}
         ctx.fillText(state[my_team][my_int][0].H.HP,x_offset-40, y_offset-5 - (0 | state[my_team][my_int][0].H.HP/2) )
+        if(state[my_team][my_int][0].L.HP==100){ctx.font = '5px monospace';}
+        else{ctx.font = '8px monospace';}
         ctx.fillText(state[my_team][my_int][0].L.HP,x_offset-30, y_offset-5 - (0 | state[my_team][my_int][0].L.HP/2) )
+        if(state[my_team][my_int][0].R.HP==100){ctx.font = '5px monospace';}
+        else{ctx.font = '8px monospace';}
         ctx.fillText(state[my_team][my_int][0].R.HP,x_offset-20, y_offset-5 - (0 | state[my_team][my_int][0].R.HP/2) )
+        if(state[my_team][my_int][0].B.HP==100){ctx.font = '5px monospace';}
+        else{ctx.font = '8px monospace';}
         ctx.fillText(state[my_team][my_int][0].B.HP,x_offset-10, y_offset-5 - (0 | state[my_team][my_int][0].B.HP/2) )
         ctx.font = '11px monospace';
 

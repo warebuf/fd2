309b531b8d25fe3135ef6ff0a6bc6115c7103094
warebuf <13jeremyjlee@gmail.com>
2024-07-05 22:05:25 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -1499,9 +1499,9 @@ function drawTable2() {
         }
         ser = 'B' + ser
         ctx.textAlign = "left";
-        ctx.fillText(ser,x_offset-10+0, y_offset+30-130 )
-        ctx.fillText(state[my_team][my_int][0].H.NAME,x_offset-10+0, y_offset+40-130 )
-        ctx.fillText("Basic close attack",x_offset-10+0, y_offset+50-130 )
+        ctx.fillText(ser,x_offset-15+0, y_offset+30-130 )
+        ctx.fillText(state[my_team][my_int][0].H.NAME,x_offset-15+0, y_offset+40-130 )
+        ctx.fillText("Basic close attack",x_offset-15+0, y_offset+50-130 )
 
 
     }

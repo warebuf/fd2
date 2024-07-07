0f87bfb45b164cbd7a50717a534352662e276281
warebuf <13jeremyjlee@gmail.com>
2024-07-06 20:36:22 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -1891,7 +1891,7 @@ function drawStartersTable() {
                     ctx.fillRect(x_offset-1-30+(user_shift*user_count),y_offset - (0 | state[j][k][i].L.HP/2) + (i*unit_shift),3,0 | state[j][k][i].L.HP/2);
                     ctx.fillRect(x_offset-1-15+(user_shift*user_count),y_offset - (0 | state[j][k][i].R.HP/2) + (i*unit_shift),3,0 | state[j][k][i].R.HP/2);
                     ctx.fillRect(x_offset-1-0+(user_shift*user_count),y_offset - (0 | state[j][k][i].B.HP/2) + (i*unit_shift),3,0 | state[j][k][i].B.HP/2);
-                    ctx.fillText("HP",x_offset-22.5+(user_shift*user_count), y_offset+35 + (i*unit_shift) )
+                    ctx.fillText("HP",x_offset-22.5+(user_shift*user_count), y_offset-50 + (i*unit_shift) )
                     ctx.font = '9px monospace';
                     ctx.fillText(state[j][k][i].H.HP,x_offset-45+(user_shift*user_count), y_offset+10 + (i*unit_shift) )
                     ctx.fillText(state[j][k][i].L.HP,x_offset-30+(user_shift*user_count), y_offset+10 + (i*unit_shift) )

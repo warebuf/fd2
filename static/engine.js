9d8f7bc4d505e3363087d02ab0ec372bcabd1718
warebuf <13jeremyjlee@gmail.com>
2024-07-08 09:41:14 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -356,6 +356,8 @@ function drawPos() {
                 ctx.fillText(road, 150, ((i+j)*115) + 102 + (k*20));
 
                 ctx.fillRect(700+(150*i),400+(k*10)+(j*60),100,1);
+                ctx.fillText('o', 700+(150*i), 400+(k*10)+(j*60) );
+                ctx.fillText('x', 700+(150*i)+100, 400+(k*10)+(j*60) );
             }
         }
     }

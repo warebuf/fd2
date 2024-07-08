4134faf598784d8d92ca6def3b5c99b6df2432c5
warebuf <13jeremyjlee@gmail.com>
2024-07-08 10:54:34 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -373,7 +373,9 @@ function drawPos() {
         }
     }
 
-
+    drawCircle(100,100,10)
+    drawPoint(100,100,10,0,'a')
+    drawPoint(100,100,10,180,'b')
 
 }
 
@@ -2094,4 +2096,22 @@ function drawStartersTable() {
         ctx.fillStyle = 'black';
 
     }
+}
+
+function drawCircle(center_x,center_y,radius){
+    ctx.beginPath();
+    ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function drawPoint(center_x,center_y,radius,angle,distance,label){
+    var x = center_x + radius * Math.cos(-angle*Math.PI/180) * distance;
+    var y = center_y + radius * Math.sin(-angle*Math.PI/180) * distance;
+
+    ctx.beginPath();
+    ctx.arc(x, y, point_size, 0, 2 * Math.PI);
+    ctx.fill();
+
+    ctx.font = font_size;
+    ctx.fillText(label,x + 10,y);
 }

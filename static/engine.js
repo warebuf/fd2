d2c11ca834ee37be0a50454c3b537d5c63d8e4fd
warebuf <13jeremyjlee@gmail.com>
2024-07-09 15:36:22 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -376,7 +376,7 @@ function drawPos() {
                     ctx.fillStyle = 'white';
                     ctx.textAlign = "left";
                     ctx.fillRect(700+(270*i)+250-(250*(state[i][j][k].Position/100)), 400+(k*120)+(j*700),1,4 );
-                    ctx.fillText(state[i][j][k].Position, 700+(270*i)+250-(250*(state[i][j][k].Position/100))-5, 400+(k*120)+(j*700)+20 );
+                    ctx.fillText(state[i][j][k].Position.toFixed(1), 700+(270*i)+250-(250*(state[i][j][k].Position/100))-5, 400+(k*120)+(j*700)+20 );
                 }
 
             }

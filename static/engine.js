f3e7d0afc91dbae713c18765d861b3e27b576e31
warebuf <13jeremyjlee@gmail.com>
2024-07-10 02:10:20 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -403,33 +403,41 @@ function drawState() {
         for(let j = 0; j < state[i].length; j++) {
             for(let k = 0; k < state[i][j].length; k++) {
                 if (state[i][j][k].H.HP > 0) {
+                    ctx.fillStyle = 'white';
+                    ctx.fillText("%", x0+(space*i),  y0-9 + (k*120)+(j*700) );
                     if(i==my_team && j==my_int) {ctx.fillStyle = 'blue';}
                     else{ctx.fillStyle = 'green';}
-                    ctx.fillText("%", x0+(space*i),  y0-9 + (k*120)+(j*700) );
+
                 } else {
                     ctx.fillStyle = 'red';
                 }
                 ctx.fillRect(x0+(space*i), y0-9 + (k*120)+(j*700), 5, 5);
                 if (state[i][j][k].L.HP > 0) {
+                    ctx.fillStyle = 'white';
+                    ctx.fillText("%", x0-5+(space*i),  y0-4 + (k*120)+(j*700) );
                     if(i==my_team && j==my_int) {ctx.fillStyle = 'blue';}
                     else{ctx.fillStyle = 'green';}
-                    ctx.fillText("%", x0-5+(space*i),  y0-4 + (k*120)+(j*700) );
+
                 } else {
                     ctx.fillStyle = 'red';
                 }
                 ctx.fillRect(x0-5+(space*i), y0-4 + (k*120)+(j*700), 5, 7); // make it 270,5,7 OR 269,6,4
                 if (state[i][j][k].R.HP > 0) {
+                    ctx.fillStyle = 'white';
+                    ctx.fillText("%", x0+5+(space*i),  y0-4 + (k*120)+(j*700) );
                     if(i==my_team && j==my_int) {ctx.fillStyle = 'blue';}
                     else{ctx.fillStyle = 'green';}
-                    ctx.fillText("%", x0+5+(space*i),  y0-4 + (k*120)+(j*700) );
+
                 } else {
                     ctx.fillStyle = 'red';
                 }
                 ctx.fillRect(x0+5+(space*i), y0-4 + (k*120)+(j*700), 5, 7);
                 if (state[i][j][k].B.HP > 0) {
+                    ctx.fillStyle = 'white';
+                    ctx.fillText("%", x0+(space*i),  y0+1 + (k*120)+(j*700) );
                     if(i==my_team && j==my_int) {ctx.fillStyle = 'blue';}
                     else{ctx.fillStyle = 'green';}
-                    ctx.fillText("%", x0+(space*i),  y0+1 + (k*120)+(j*700) );
+
                 } else {
                     ctx.fillStyle = 'red';
                 }

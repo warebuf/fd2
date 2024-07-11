26f331f50f58df43391d55cf278f78c2c9686bb3
warebuf <13jeremyjlee@gmail.com>
2024-07-10 20:18:15 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -2307,25 +2307,25 @@ function drawStartersTable2() {
 
                     ctx.font = '8px monospace';
                     if(state[j][k][i].H.HP>0) {
-                        ctx.fillText((0|(100*state[j][k][i].H.Weight/total_prob))+"%", x_offset+445+(user_shift*user_count),  y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].H.Weight/2) );
+                        ctx.fillText('('+(0|(100*state[j][k][i].H.Weight/total_prob))+")%", x_offset+445+(user_shift*user_count),  y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].H.Weight/2) );
                         ctx.fillText(state[j][k][i].H.Weight,x_offset+445+(user_shift*user_count), y_offset-20 + (i*unit_shift) - (0 | state[j][k][i].H.Weight/2) )
                     } else {
                         ctx.fillText(state[j][k][i].H.Weight,x_offset+445+(user_shift*user_count), y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].H.Weight/2) )
                     }
                     if (state[j][k][i].L.HP > 0) {
-                        ctx.fillText((0|(100*state[j][k][i].L.Weight/total_prob))+"%", x_offset+460+(user_shift*user_count),  y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].L.Weight/2) );
+                        ctx.fillText('('+(0|(100*state[j][k][i].L.Weight/total_prob))+")%", x_offset+460+(user_shift*user_count),  y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].L.Weight/2) );
                         ctx.fillText(state[j][k][i].L.Weight,x_offset+460+(user_shift*user_count), y_offset-20 + (i*unit_shift) - (0 | state[j][k][i].L.Weight/2) )
                     } else {
                         ctx.fillText(state[j][k][i].H.Weight,x_offset+445+(user_shift*user_count), y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].H.Weight/2) )
                     }
                     if (state[j][k][i].R.HP > 0) {
-                        ctx.fillText((0|(100*state[j][k][i].R.Weight/total_prob))+"%", x_offset+475+(user_shift*user_count),  y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].R.Weight/2) );
+                        ctx.fillText('('+(0|(100*state[j][k][i].R.Weight/total_prob))+")%", x_offset+475+(user_shift*user_count),  y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].R.Weight/2) );
                         ctx.fillText(state[j][k][i].R.Weight,x_offset+475+(user_shift*user_count), y_offset-20 + (i*unit_shift) - (0 | state[j][k][i].R.Weight/2) )
                     } else {
                         ctx.fillText(state[j][k][i].R.Weight,x_offset+475+(user_shift*user_count), y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].R.Weight/2) )
                     }
                     if (state[j][k][i].B.HP > 0) {
-                        ctx.fillText((0|(100*state[j][k][i].B.Weight/total_prob))+"%", x_offset+490+(user_shift*user_count),  y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].B.Weight/2) );
+                        ctx.fillText('('+(0|(100*state[j][k][i].B.Weight/total_prob))+")%", x_offset+490+(user_shift*user_count),  y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].B.Weight/2) );
                         ctx.fillText(state[j][k][i].B.Weight,x_offset+490+(user_shift*user_count), y_offset-20 + (i*unit_shift) - (0 | state[j][k][i].B.Weight/2) )
                     } else {
                         ctx.fillText(state[j][k][i].B.Weight,x_offset+490+(user_shift*user_count), y_offset-10 + (i*unit_shift) - (0 | state[j][k][i].B.Weight/2) )

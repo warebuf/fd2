32f6548947d3b35f837dd30ef1cd314e50f0de5f
warebuf <13jeremyjlee@gmail.com>
2024-07-10 23:07:12 -0600

    working on char sel

--- static/engine.js
+++ static/engine.js
@@ -1923,8 +1923,8 @@ function drawBenchTable() {
 
         let x_offset = 1000
         let y_offset = 190
-        let unit_shift = 170
-        let second_slot_shift = 255
+        let unit_shift = 155
+        let second_slot_shift = 270
         ctx.font = '11px monospace';
         ctx.fillStyle = 'white';
 

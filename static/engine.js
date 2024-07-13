4b1cdd5e8f92ca78311b1108a513bce652d69d41
warebuf <13jeremyjlee@gmail.com>
2024-07-12 23:21:54 -0600

    changed to parts object

--- static/engine.js
+++ static/engine.js
@@ -1970,5 +1970,8 @@ function drawImage(){
     img = new Image();
 
     img.src = "/images/yugioh1.1.jpg"
-    ctx.drawImage(img, 20,20);
+    img.onload = function () {
+        ctx.drawImage(img, 20,20);
+    };
+
 }

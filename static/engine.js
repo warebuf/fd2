36b25b840cf7b1a62cda693cb1b3f40676609863
warebuf <13jeremyjlee@gmail.com>
2024-07-20 12:06:30 -0600

    draw board

--- static/engine.js
+++ static/engine.js
@@ -471,7 +471,9 @@ function drawState() {
                                             let x = Math.cos((2*Math.PI*(count)/10) + ((4)*(Math.PI)/10) ) ;
                                             let y = Math.sin((2*Math.PI*(count)/10) + ((4)*(Math.PI)/10) ) ;
 
-                                            console.log(x,y, c.width, c.height)
+                                            let test1 = c.width/2
+                                            let test2 = c.height/2
+                                            console.log(x,y, test1, test2)
 
                                             ctx.beginPath();
                                             ctx.moveTo( (x*250)+(c.width/2), (y*250)+(c.height/2) );

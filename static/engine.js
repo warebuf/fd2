ca20b4500508563826f8c9389bd1d6a17ac239d2
warebuf <13jeremyjlee@gmail.com>
2024-07-20 10:06:45 -0600

    draw board

--- static/engine.js
+++ static/engine.js
@@ -2075,9 +2075,8 @@ function calculateCenter(center_x,center_y) {
 
                     sum_x = sum_x + x
                     sum_y = sum_y + y
-                    count = count + 1
-
                 }
+                count = count + 1
 
             }
         }

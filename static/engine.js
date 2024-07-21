864f2c12965f38248857a258b054b6430e75c798
warebuf <13jeremyjlee@gmail.com>
2024-07-20 19:01:21 -0600

    draw board

--- static/engine.js
+++ static/engine.js
@@ -2162,7 +2162,7 @@ function drawHeatMap(){
                 ctx.fillText(state[i][j][k].H.HP,x_offset+x_shift,y_offset+y_shift+10);
                 x_shift = x_shift+20;
 
-                ctx.textAlign = "left";
+                ctx.textAlign = "center";
                 ctx.fillStyle = 'blue';
                 ctx.fillRect(x_offset+x_shift,y_offset+y_shift,10,10)
                 ctx.font = '11px monospace';

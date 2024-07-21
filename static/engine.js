d31083086a9a7830d68348f2291c58c27df93149
warebuf <13jeremyjlee@gmail.com>
2024-07-20 20:26:12 -0600

    draw board

--- static/engine.js
+++ static/engine.js
@@ -2153,7 +2153,7 @@ function drawHeatMap(){
     let y_shift = 0;
 
     ctx.textAlign = "center";
-    ctx.font = '8px monospace';
+    ctx.font = '11px monospace';
     ctx.fillText("HP",x_offset+x_shift+10,y_offset+y_shift-10);
     x_shift = x_shift+20
     ctx.fillText("DEF",x_offset+x_shift+10,y_offset+y_shift-10);

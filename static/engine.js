560a5bdb2a87c846f76eb893bdacdaec006a78d9
warebuf <13jeremyjlee@gmail.com>
2024-07-21 20:48:24 -0600

    working on heat chart

--- static/engine.js
+++ static/engine.js
@@ -2162,26 +2162,56 @@ function drawHeatMap(){
     for(let i = 0; i < state.length; i++) {
 
         ctx.textAlign = "center";
+
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(x_offset+x_shift+t_shift,y_offset+y_shift,24,19)
         ctx.fillStyle = 'white';
         ctx.font = '10px monospace';
         ctx.fillText("HP",x_offset+x_shift+t_shift+12,y_offset+y_shift-5);
         x_shift = x_shift+25
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(x_offset+x_shift+t_shift,y_offset+y_shift,24,19)
+        ctx.fillStyle = 'white';
         ctx.fillText("DEF",x_offset+x_shift+t_shift+12,y_offset+y_shift-5);
         x_shift = x_shift+25
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(x_offset+x_shift+t_shift,y_offset+y_shift,24,19)
+        ctx.fillStyle = 'white';
         ctx.fillText("DOG",x_offset+x_shift+t_shift+12,y_offset+y_shift-5);
         x_shift = x_shift+25
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(x_offset+x_shift+t_shift,y_offset+y_shift,24,19)
+        ctx.fillStyle = 'white';
         ctx.fillText("ATK",x_offset+x_shift+t_shift+12,y_offset+y_shift-5);
         x_shift = x_shift+25
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(x_offset+x_shift+t_shift,y_offset+y_shift,24,19)
+        ctx.fillStyle = 'white';
         ctx.fillText("ACC",x_offset+x_shift+t_shift+12,y_offset+y_shift-5);
         x_shift = x_shift+25
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(x_offset+x_shift+t_shift,y_offset+y_shift,24,19)
+        ctx.fillStyle = 'white';
         ctx.fillText("CRT",x_offset+x_shift+t_shift+12,y_offset+y_shift-5);
         x_shift = x_shift+25
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(x_offset+x_shift+t_shift,y_offset+y_shift,24,19)
+        ctx.fillStyle = 'white';
         ctx.fillText("CD",x_offset+x_shift+t_shift+12,y_offset+y_shift-5);
         x_shift = x_shift+25
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(x_offset+x_shift+t_shift,y_offset+y_shift,24,19)
+        ctx.fillStyle = 'white';
         ctx.fillText("CLU",x_offset+x_shift+t_shift+12,y_offset+y_shift-5);
         x_shift = x_shift+25
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(x_offset+x_shift+t_shift,y_offset+y_shift,24,19)
+        ctx.fillStyle = 'white';
         ctx.fillText("WGT",x_offset+x_shift+t_shift+12,y_offset+y_shift-5);
         x_shift = x_shift+25
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(x_offset+x_shift+t_shift,y_offset+y_shift,24,19)
+        ctx.fillStyle = 'white';
         ctx.fillText("SPD",x_offset+x_shift+t_shift+12,y_offset+y_shift-5);
         x_shift = 0
 

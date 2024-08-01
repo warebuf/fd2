a6a0ee4e9f0521e8c1d1a6fc40f4859485e93614
warebuf <13jeremyjlee@gmail.com>
2024-08-01 08:23:36 -0600

    doing dmg calc

--- static/engine.js
+++ static/engine.js
@@ -340,8 +340,15 @@ function drawPos()  {
 
             for(let j = 0 ; j < atk_json_obj.Damage.length; j++) { //
                 let text = atk_json_obj.Damage[j][0].split(";")
+                let dmg = text[1]
+                let hw = text[2]
+                let lw = text[3]
+                let rw = text[4]
+                let bw = text[5]
+                let dice = text[6]
 
                 ctx.fillText(atk_i.toString()+atk_j.toString()+atk_k.toString()+text[0] + "->" + def_i+def_j+def_k, 1000, 110 + (100*i) + (10*j) );
+                ctx.fillText(hw/(hw+lw+rw+bw), 1000, 120 + (100*i) + (20*j) );
 
 
             }
